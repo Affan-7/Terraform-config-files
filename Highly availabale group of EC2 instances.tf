15098160d65c01734d8b31c92c36aecf6ca25baa
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-01-22 10:20:41 +0530

    Added public_c subnet inside HA EC2

--- Highly availabale group of EC2 instances.tf
+++ Highly availabale group of EC2 instances.tf
@@ -40,3 +40,13 @@ resource "aws_subnet" "public_b" {
     Name = "public_b"
   }
 }
+
+resource "aws_subnet" "public_c" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "public_c"
+  }
+}
