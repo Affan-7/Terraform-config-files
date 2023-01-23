3f629977f55ab1e1eda8642fac6d364847dffbeb
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-01-23 10:58:31 +0530

    "Added internet gateway inside HA EC2"

--- Highly availabale group of EC2 instances.tf
+++ Highly availabale group of EC2 instances.tf
@@ -50,3 +50,11 @@ resource "aws_subnet" "public_c" {
     Name = "public_c"
   }
 }
+
+resource "aws_internet_gateway" "main-igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
