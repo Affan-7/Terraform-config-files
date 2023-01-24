2580d13d5b6dbbd2940f31d84273621574858fb2
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-01-24 15:41:44 +0530

    Added public route table inside HA EC2

--- Highly availabale group of EC2 instances.tf
+++ Highly availabale group of EC2 instances.tf
@@ -58,3 +58,16 @@ resource "aws_internet_gateway" "main-igw" {
     Name = "main-igw"
   }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
