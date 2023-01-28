50c3b0a189f8881c28004b79fdeadd676fffd3d4
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-01-28 10:03:48 +0530

    Added rt association with subnet 'c' inside HA EC2

--- Highly availabale group of EC2 instances.tf
+++ Highly availabale group of EC2 instances.tf
@@ -81,3 +81,8 @@ resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_route_table_association" "c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public_rt.id
+}
