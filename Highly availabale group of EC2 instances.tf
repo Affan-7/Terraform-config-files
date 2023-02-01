e91270ae01e101eb9eecb347ee1c045d951a5808
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-02-01 14:14:10 +0530

    Added LB target group inside HA EC2

--- Highly availabale group of EC2 instances.tf
+++ Highly availabale group of EC2 instances.tf
@@ -138,3 +138,11 @@ resource "aws_lb_listener" "my_lb_listener" {
     target_group_arn = aws_lb_target_group.my_tg.arn
   }
 }
+
+resource "aws_lb_target_group" "my_tg" {
+  name     = "my-tg"
+  target_type = "instance"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
