865e79f9fdd796f957701d52cc3ab5c4c25711e6
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-02-04 14:09:03 +0530

    Added autoscaling group inside HA EC2

--- Highly availabale group of EC2 instances.tf
+++ Highly availabale group of EC2 instances.tf
@@ -171,3 +171,19 @@ resource "aws_launch_template" "my_launch_template" {
     security_groups = [aws_security_group.my_sg.id]
   }
 }
+
+resource "aws_autoscaling_group" "my_asg" {
+  name                      = "my_asg"
+  max_size                  = 5
+  min_size                  = 2
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  target_group_arns = [aws_lb_target_group.my_tg.arn]
+
+  vpc_zone_identifier       = [aws_subnet.public_a.id, aws_subnet.public_b.id, aws_subnet.public_c.id]
+  
+  launch_template {
+    id      = aws_launch_template.my_launch_template.id
+    version = "$Latest"
+  }
+}
