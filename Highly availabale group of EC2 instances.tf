ee4ecd6178712683df3af421a9ee988793600d1f
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-02-06 17:26:09 +0530

    Added autoscaling policy inside HA EC2

--- Highly availabale group of EC2 instances.tf
+++ Highly availabale group of EC2 instances.tf
@@ -187,3 +187,12 @@ resource "aws_autoscaling_group" "my_asg" {
     version = "$Latest"
   }
 }
+
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "scale_up"
+  policy_type            = "SimpleScaling"
+  autoscaling_group_name = aws_autoscaling_group.my_asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "1"    # add one instance
+  cooldown               = "300"  # cooldown period after scaling
+}
