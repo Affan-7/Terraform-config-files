051756ad68c16afa556e36a8d89c95bddc6e1bd9
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-02-15 10:42:57 +0530

    Added private subnet 'a'

--- EKS cluster.tf
+++ EKS cluster.tf
@@ -26,3 +26,15 @@ resource "aws_internet_gateway" "eks_igw" {
     Name = "eks_igw"
   }
 }
+
+resource "aws_subnet" "eks_private_us_east_1a" {
+  vpc_id     = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "eks_private_us_east_1a"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/cluster-name" = "shared"
+  }
+}
