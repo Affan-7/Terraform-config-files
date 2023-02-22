25329cb5c575dc1e215640c00ea72cbdaa1218a5
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-02-22 16:07:39 +0530

    Added NAT gateway

--- EKS cluster.tf
+++ EKS cluster.tf
@@ -59,3 +59,14 @@ resource "aws_eip" "eks_nat_eip" {
     Name = "eks_nat_eip"
   }
 }
+
+resource "aws_nat_gateway" "eks_nat" {
+  allocation_id = aws_eip.eks_nat_eip.id
+  subnet_id     = aws_subnet.eks_public_us_east_1a.id
+
+  tags = {
+    Name = "eks_nat"
+  }
+
+  depends_on = [aws_internet_gateway.eks_igw]
+}
